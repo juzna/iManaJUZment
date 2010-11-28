98668257b27b06c3d79f2a354bd77f160ceaa037
Jan Dolecek <juzna.cz@gmail.com>
2010-11-28 21:35:08 +0000

    small changes

---
+++ scripts/init.php
@@ -0,0 +1,10 @@
+<?php
+// absolute filesystem path to the web root
+define('APP_DIR', __DIR__ . '/../app');
+
+// absolute filesystem path to the libraries
+define('LIBS_DIR', APP_DIR . '/../libs');
+
+// load bootstrap file
+require APP_DIR . '/bootstrap-common.php';
+
