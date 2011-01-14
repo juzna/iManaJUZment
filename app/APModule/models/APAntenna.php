d9b1c2743103b7ec41f3f48d25c2d9e9156955a3
Jan Dolecek <juzna.cz@gmail.com>
2011-01-14 00:21:56 +0000

    added licence to PHP files

--- app/APModule/models/APAntenna.php
+++ app/APModule/models/APAntenna.php
@@ -1,5 +1,21 @@
 <?php
 /**
+ * This file is part of the "iManaJUZment" - complex system for internet service providers
+ *
+ * Copyright (c) 2005 - 2011 Jan Dolecek (http://juzna.cz)
+ *
+ * iManaJUZment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iManaJUZment.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.txt
+ */
+
+/**
  * Úhel se udává ve stupních při pohledu na mapu. Na východé je 0, na severu 90, západ 180, jih 270.<br />
  * Směr udává kam směruje anténa při pohledu na mapu. Rozsah je úhel, ve kterém vyzařuje signal. <br />
  * Dosah udává poloměr kruhové výseče v metrech, kde by měl být příjem bez problému.<br />
--- app/classes/tables/DoctrineEntityTableDefinition.php
+++ app/classes/tables/DoctrineEntityTableDefinition.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * This file is part of the "iManaJUZment" - complex system for internet service providers
+ *
+ * Copyright (c) 2005 - 2011 Jan Dolecek (http://juzna.cz)
+ *
+ * iManaJUZment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iManaJUZment.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.txt
+ */
+
 
 namespace Tables;
 use Doctrine,
