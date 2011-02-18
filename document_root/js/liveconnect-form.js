f4b42e0ab99219d8c24b065d35226dbf01a21002
Jan Dolecek <juzna.cz@gmail.com>
2011-02-18 21:06:19 +0000

    fixed licence in some files

--- document_root/js/liveconnect-form.js
+++ document_root/js/liveconnect-form.js
@@ -1,9 +1,17 @@
 /**
- * Created by JetBrains PhpStorm.
- * User: juzna
- * Date: 2/17/11
- * Time: 8:06 PM
- * To change this template use File | Settings | File Templates.
+ * This file is part of the "iManaJUZment" - complex system for internet service providers
+ *
+ * Copyright (c) 2005 - 2011 Jan Dolecek (http://juzna.cz)
+ *
+ * iManaJUZment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with iManaJUZment.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.txt
  */
 
 // Add handlers
